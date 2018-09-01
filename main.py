b225a7a024edf0b4ac9589ef2652de5323f5a5bc
Brian <brianxu01@gmail.com>
2018-09-01 11:56:19 -0700

    gae update

--- main.py
+++ main.py
@@ -23,27 +23,26 @@ class FHSCSCRequests(object):
         return handler.threaded_select()
 
 
-if __name__ == '__main__':
-    conf = {
-        '/': {
-            'tools.staticdir.root': os.path.abspath(os.getcwd())
-        },
-        '/submit': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tools.response_headers.on': True,
-            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
-        },
-        '/static': {
-            'tools.staticdir.on': True,
-            'tools.staticdir.dir': './public'
-        }
+conf = {
+    '/': {
+        'tools.staticdir.root': os.path.abspath(os.getcwd())
+    },
+    '/submit': {
+        'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+        'tools.response_headers.on': True,
+        'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+    },
+    '/static': {
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': './public'
     }
+}
 
-    db = 'messages.db'
-    handler = SQLHandler(db)
+db = 'messages.db'
+handler = SQLHandler(db)
 
-    webapp = FHSCSC()
-    webapp.submit = FHSCSCRequests()
-    # Uncomment this line and comment out the 2 below if you're running locally
-    # cherrypy.quickstart(webapp, '/', conf)
-    app = cherrypy.tree.mount(FHSCSC(), '/', conf)
+webapp = FHSCSC()
+webapp.submit = FHSCSCRequests()
+# Uncomment this line and comment out the 2 below if you're running locally
+# cherrypy.quickstart(webapp, '/', conf)
+app = cherrypy.tree.mount(FHSCSC(), '/', conf)
